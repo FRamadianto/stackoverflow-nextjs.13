63b5e60c895fa6bd7161d424f94218f8c510e1ca
Fikri Ramadianto <ramadiantofikri@gmail.com>
2023-10-31 00:49:07 +0700

    fixing webhooks

--- next.config.js
+++ next.config.js
@@ -4,6 +4,18 @@ const nextConfig = {
         serverActions: true,
         mdxRss: true,
         serverComponentsExternalPackages:['mongoose']
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*',
+            },
+            {
+                protocol: 'http',
+                hostname: '*',
+            }
+        ]
     }
 }
 
